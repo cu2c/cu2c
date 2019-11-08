e88b5569d0df411ee7b86e02a44864b07bda1e17
Mátyás Aradi <aradi.matyas@wigner.mta.hu>
2019-11-08 14:10:57 +0100

    preparation for cudaMemcpy

--- commandList.sh
+++ commandList.sh
@@ -1,6 +1,8 @@
 sed -i 's/cudaFree/free/g' $1
 sed -i 's:__global__\s:// Global CUDA function\n:g' $1
-#sed 's' $1
+sed -i 's:idx:cu2c_idx:g' $1
+sed -i 's: [iI][dD][xX]:cu2c_idx:g' $1
+#sed -i 's:cudaMemcpy(*,*,*):\0 = \1:g' $1
 echo "-------------"
 sed '' $1
 
