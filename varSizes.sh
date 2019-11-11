8e2ab775cb93dfdcfe09f23088c58fc0118fb1da
Mátyás Aradi <aradi.matyas@wigner.mta.hu>
2019-11-11 15:48:58 +0100

    varSizes made from varTypes.sh

---
+++ varSizes.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+while IFS='' read -r line || [[ -n "$line" ]]; do
+    first=$(echo $line | awk '{print $1;}')
+
+    if [[ $first = "double" || $first = "float" || $first = "long" || $first = "int" || $first = "char" ]]; then
+
+        echo -e "-----" #"$first     \t>>> $line"
+	lastchar=","
+	i=2;
+        while [[ $lastchar == "," ]]; do
+            next=$(echo $line | awk '{print $'$i';}')
+            firstchar=$(echo "${next:0:1}")
+            lastchar=$(echo "${next: -1}")
+            varname=""
+
+            while [[ $firstchar == "*" ]]; do
+                varname=$varname"p_"
+                next=$(echo "${next: 1}")
+                firstchar=$(echo "${next:0:1}")
+            done
+
+            if [[ $lastchar = "," || $lastchar == ";" ]]; then
+                next=$(echo "${next:0:-1}")
+            fi
+            varname=$varname$next
+
+            echo -e "$varname: $first"
+            eval "cu2c_type_$varname=$first"
+            i=`expr $i + 1`
+        done
+    fi
+done < "$1"
+
+echo "cu2c: $cu2c_type_p_hArr"
