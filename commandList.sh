1cef3dece91352156e160f0cb64f073a6325abbc
Mátyás Aradi <aradi.matyas@wigner.mta.hu>
2019-11-13 17:54:28 +0100

    CUDA array allocation added

--- commandList.sh
+++ commandList.sh
@@ -3,7 +3,7 @@ sed -i 's:__global__\s:// Global CUDA function\n:g' $1
 sed -i 's:idx:cu2c_idx:g' $1
 sed -i 's: [iI][dD][xX]:cu2c_idx:g' $1
 sed -Ei "s:(cudaMemcpy\()([^,]*),([^,]*),([^;]*)(.*):\2 =\3\5:" $1
-./varSizes.sh $1
+./cu2cAlloc.sh $1
 echo "-------------"
 sed '' $1
 
