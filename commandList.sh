a6b768e8f59fa35867b7027933b66aa290c8119c
Mátyás Aradi <aradi.matyas@wigner.mta.hu>
2019-11-19 05:33:02 +0100

    Bug fix: global function variables are accepted

--- commandList.sh
+++ commandList.sh
@@ -1,11 +1,11 @@
-űsed -i 's/cudaFree/free/g' $file
+sed -i 's/cudaFree/free/g' $file
 sed -Ei 's:(__global__)([^\(])\((.*):// Global CUDA function\n\1\3:g' $file
 sed -i 's:[iI][dD][xX]:cu2c_idx:g' $file
 sed -i 's:int cu2c\_idx[ ]*=[^;]*;:/*IDX definition removed*/:g' $file
 sed -i 's:long cu2c\_idx[ ]*=[^;]*;:/*IDX definition removed*/:g' $file
 sed -Ei "s:(cudaMemcpy\()([^,]*),([^,]*),([^;]*)(.*):\2 =\3\5:" $file
 source cu2cAlloc.sh
-sed -Ei 's:([ ]*)([a-zA-Z0-9_]*[ ]*)(<<<[ ]*)([a-zA-Z0-9_\.]*)([ ]*,[ ]*)([a-zA-Z0-9_\.]*)([ ]*>>>[ ]*)(\(.*;)(.*):\n\1for(long cu2c_idx = 0; cu2c_idx < \4*\6 ; ++cu2c_idx){\n\1    \2\8\n\1}\9\n:g' $file
+sed -Ei 's:([ ]*)([a-zA-Z0-9_]*[ ]*)(<<<[ ]*)([a-zA-Z0-9_\.]*)[ ]*,[ ]*([a-zA-Z0-9_\.]*)([ ]*>>>[ ]*)(\()(.*;)(.*):\n\1for(long cu2c_idx = 0; cu2c_idx < \4*\5 ; ++cu2c_idx){\n\1    \2(cu2c_idx, \8\n\1}\9\n:g' $file
 sed -Ei 's:([^_]*)(__global__ )([^(]*\()(.*):// Global CUDA function\n\1\3long cu2c_idx, \4:g' $file
 sed -Ei 's:(__device__)\s:// Device CUDA function\n:g' $file
 
