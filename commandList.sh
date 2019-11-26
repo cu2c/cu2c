d177cb91799fa49918a0c8c44f1de89fae95d45f
Mátyás Aradi <aradi.matyas@wigner.mta.hu>
2019-11-26 09:53:50 +0100

    Fix: idx and curand_uniform

--- commandList.sh
+++ commandList.sh
@@ -1,8 +1,8 @@
 sed -i 's/cudaFree/free/g' $file
 sed -Ei 's:(__global__)([^\(])\((.*):// Global CUDA function\n\1\3:g' $file
+sed -i 's:int [iI][dD][xX][ ]*=[ ]*[^;0-9]*;:/*I D X definition removed*/:g' $file
+sed -i 's:long [iI][dD][xX][ ]*=[ ]*[^;0-9]*;:/*I D X definition removed*/:g' $file
 sed -i 's:[iI][dD][xX]:cu2c_idx:g' $file
-sed -i 's:int cu2c\_idx[ ]*=[^;]*;:/*IDX definition removed*/:g' $file
-sed -i 's:long cu2c\_idx[ ]*=[^;]*;:/*IDX definition removed*/:g' $file
 sed -Ei "s:(cudaMemcpy\()([^,]*),([^,]*),([^;]*)(.*):\2 =\3\5:" $file
 source cu2cAlloc.sh
 sed -Ei 's:([ ]*)([a-zA-Z0-9_]*[ ]*)(<<<[ ]*)([a-zA-Z0-9_\.]*)[ ]*,[ ]*([a-zA-Z0-9_\.]*)([ ]*>>>[ ]*)(\()(.*;)(.*):\n\1for(long cu2c_idx = 0; cu2c_idx < \4*\5 ; ++cu2c_idx){\n\1    \2(cu2c_idx, \8\n\1}\9\n:g' $file
@@ -13,8 +13,8 @@ sed -i 's:#include <cuda.*://\0:g' $file
 sed -i 's:#include <curand.*://\0:g' $file
 sed -i 's:curandState[^;]*;::g' $file
 sed -i 's:curand_init[^;]*:srand(time(NULL)):g' $file
-sed -i 's:curand_uniform_double[^;]*:(double)rand() / (double)RAND_MAX:g' $file
-sed -i 's:curand_uniform[^;]*:(float)rand() / (float)RAND_MAX:g' $file
+sed -i 's:curand_uniform_double([^)]*):(double)rand() / (double)RAND_MAX:g' $file
+sed -i 's:curand_uniform([^)]*):(float)rand() / (float)RAND_MAX:g' $file
 sed -Ei "s:(\#include[^.]*)(\.cuh):\1$CU2C_CUH:g" $file
 sed -Ei "s:(\#include[^.]*)(\.cu):\1$CU2C_CU:g" $file
 sed -i "s:cudaGetLastError():true:g" $file
