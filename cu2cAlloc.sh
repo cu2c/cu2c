6dfd7730b6de10a813b29e677161d5cf22936bd2
Mátyás Aradi <aradi.matyas@wigner.mta.hu>
2019-11-27 03:18:23 +0100

    Bug fix: cudaMalloc size_t name: full equivalence required

--- cu2cAlloc.sh
+++ cu2cAlloc.sh
@@ -11,9 +11,12 @@ while read -r line; do
         s=$(echo "$s" | tr -d " \t")
         varname=$(echo "$s" | sed -E "s:([^=].+)=.+(.*):\1:")
         vartype=$(echo "$s" | sed -E "s:([^=].+)=([^(]*).+\(([^)]*).+(.*):\3:")
-#echo -e "$varname\t$vartype"
         if [ $varname = $vartype ]; then
             : #echo "$varname: !ERROR!"
+        elif [ -z $vartype ]; then
+            :
+        elif [ -z $varname ]; then
+            :
         else
             varNameList+=("$varname")
             varTypeList+=("$vartype")
@@ -21,8 +24,8 @@ while read -r line; do
     done
 done < <(grep "size_t" $file )
 
-for (( i=0; i<"${#varNameList[@]}"; i++ )); do 
+for (( i=0; i<"${#varNameList[@]}"; i++ )); do
     varName=${varNameList[$i]}
     varType=${varTypeList[$i]}
-    sed -Ei "s:(.*)(cudaMalloc[ ]*\([ ]*\(*void[ ]*\*\*\)[ ]*\&)([^,]*)(,[ ]*)($varName)(.*):\1\3 = \($varType \*\) malloc\($varName\6:" $file
+    sed -Ei "s:(.*)(cudaMalloc[ ]*\([ ]*\(*void[ ]*\*\*\)[ ]*\&)([^,]*)(,[ ]*)($varName)([ ]*\).*):\1\3 = \($varType \*\) malloc\($varName\6:" $file
 done
